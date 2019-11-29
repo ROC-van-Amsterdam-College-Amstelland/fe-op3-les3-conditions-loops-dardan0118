85ce37fd6c652c82cf35139577ab72e6698be948
dardan0118 <22111704@talnet.nl>
2019-11-29 11:11:21 +0100

    opdracht 3

--- conditions_opdrachten/conditions_opdracht_3/js/script.js
+++ conditions_opdrachten/conditions_opdracht_3/js/script.js
@@ -1,1 +1,24 @@
 //javascript code
+
+
+
+
+function CheckSaldo ()
+{
+    
+    var saldo=document.getElementById("inputsaldo").value;
+    
+    var gift;
+    
+    if (saldo <= 49)
+    { 
+        document.getElementById("Mysaldo").innerHTML="saldo is te laag pik"
+    }
+    else if (saldo > 500) { 
+        document.getElementById("Mysaldo").innerHTML="saldo is goed met een bonus"
+    }
+    else 
+    {
+        document.getElementById("Mysaldo").innerHTML="saldo is goed "
+    }
+}

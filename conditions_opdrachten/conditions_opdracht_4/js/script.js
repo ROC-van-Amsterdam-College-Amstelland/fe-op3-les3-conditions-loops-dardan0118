56536a9fae054a3cbe34595387de1cbc25f61b51
dardan0118 <22111704@talnet.nl>
2019-12-05 10:21:47 +0100

    push

--- conditions_opdrachten/conditions_opdracht_4/js/script.js
+++ conditions_opdrachten/conditions_opdracht_4/js/script.js
@@ -1,1 +1,11 @@
 //javascript code
+
+
+function  MyDemo() {
+var myoutput="naam <br>";
+for (var i = 0; i < 5; i++)
+{
+    myoutput += i+"<br>";
+}
+document.getElementById("demo").innerHTML = myoutput;
+}

5053b3c94e90df88c384290589d983971b3c4ef8
dardan0118 <22111704@talnet.nl>
2019-12-09 12:58:16 +0100

    loops

--- loops_opdrachten/loops_opdracht_3/js/script.js
+++ loops_opdrachten/loops_opdracht_3/js/script.js
@@ -1,1 +1,25 @@
-//javascript code
+
+function nummers ()
+{
+    var i;
+    var MyOutput="";
+   
+    for(i=0;i<=50;i++)
+    {
+        if(i==20)
+        {
+            MyOutput+=i+" dit zijn mooie getallen <br>"
+        }
+        
+        else if(i==40)
+        {
+            MyOutput+=i+" dit zijn mooie getallen <br>"
+        }
+        
+        else {MyOutput+=i+"<br>";}
+        
+    }
+    
+    
+    document.getElementById("MyOutput").innerHTML=MyOutput;
+}
--- loops_opdrachten/loops_opdracht_4/js/script.js
+++ loops_opdrachten/loops_opdracht_4/js/script.js
@@ -1,1 +1,27 @@
-//javascript code
+
+console.log(nummers)
+
+function nummers ()
+{
+    var i;
+    var MyOutput="";
+   
+    for(i=0;i<=1000;i+=10)
+    {
+        if(i==20)
+        {
+            MyOutput+=i+" dit zijn mooie getallen <br>"
+        }
+        
+        else if(i==40)
+        {
+            MyOutput+=i+" dit zijn mooie getallen <br>"
+        }
+        
+        else {MyOutput+=i+"<br>";}
+        
+    }
+    
+    
+    document.getElementById("MyOutput").innerHTML=MyOutput;
+}
